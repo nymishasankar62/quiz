dbb4398267afece226033ca5668805ccbdf3488f
Nymisha sankar <nymishasankar62@gmail.com>
2021-11-26 16:13:14 +0530

    quiz

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "What is an applet?",
+    optionA: "An applete is a Java Program that runs in a Web browser",
+    optionB: "Applet is a standalone Java Program",
+    optionC: "Applet is a tool",
+    optionD: "Applet is a run time environment",
+    asnwer: "optionA",
+  },
+  {
+    prompt: "React.js is a free and opn-source front-end",
+    optionA: "CSS library",
+    optionB: "Bootstrap library",
+    optionC: "JavaScript library",
+    optionD: "None",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of the following command is used to create a react app?",
+    optionA: "install -g create-react-app",
+    optionB: " npm install create-react-app",
+    optionC: "npm install -g create-react-app",
+    optionD: " None of the Above",
+    asnwer: "optionc",
+  },
+];
